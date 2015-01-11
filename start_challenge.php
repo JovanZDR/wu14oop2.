ea23528f3a3a486017034a22d470a783a1a00b8c
JovanZDR <Jovanzdrave@gmail.com>
2015-01-11 21:33:05 +0100

    no message

--- start_challenge.php
+++ start_challenge.php
@@ -24,15 +24,62 @@ if (count($ds->characters) < 2) {
 $human_player = $ds->characters[0];
 
 
-if($companionIndex == 1){
+if($companionIndex){
 	$random_index_number = rand(1,2);
 	$companion = $ds->characters[$random_index_number];
 		if ($random_index_number = 1 ) {
-			$oponent = $ds->characters[2];
+			$opponent = $ds->characters[2];
 		}else{
-			$oponent = $ds->characters[1]; 
+			$opponent = $ds->characters[1]; 
 		}
+	$human_player->success -= 5;
+  	$companion->success -= 5;
 
+  
+  	$characters = array();
+  	$players[] = New Together($human_player, $companion);
+
+  	$characters[] = $opponent;
+
+  
+  	$result = $human_player->doChallengeWithFriend($ds->current_challenge[0], $human_player, $companion, $opponent);
+
+  
+  	$winner = $result[0];
+  	$last = $result[count($result)-1];
+  		if (get_class($winner) == "Together") {
+
+	    $human_player->success += 9;
+	    $companion->success += 9;
+
+	   
+	    $opponent->success -= 5;
+	    $opponent->loseTool($ds->available_items);
+
+		}
+} 	else {
+  
+	  $result = $human_player->acceptChallenge($ds->current_challenge[0], $ds->characters);
+
+	  
+	  $winner = $result[0];
+	  $last = $result[count($result)-1];
+
+	 
+	  $winner->success += 15;
+
+
+	  $last->success -= 5;
+	  $last->loseTool($ds->available_items);
 	}
+
+
+
+$echo_data = array(
+  "result" => $result,
+  "playing" => $ds->characters,
+);
+
+echo(json_encode($echo_data));
 
  
